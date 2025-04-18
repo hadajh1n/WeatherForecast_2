45dbc9ecfaf949f4083d730063ce160502e09714
hadajh1n <anton_sheludchenkov@mail.ru>
2025-04-18 11:21:04 +0300

    Обновление проекта

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,12 +37,10 @@ android {
 
 dependencies {
 
-    dependencies {
-        implementation("com.squareup.retrofit2:retrofit:2.9.0")
-        implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
-        implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1") // Для lifecycleScope
-    }
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1") // Для lifecycleScope
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
